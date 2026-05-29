b0eeb43283696f2d6ebdc511701d5808d60ad028
Guilherme <guilhermecboston@gmail.com>
2026-05-29 04:38:15 -0300

    Revert "Add RavynCore Asset Engine for OTC 1524 modern assets."
    This reverts commit 1341e8d22866ea391ac748458d2d5e92d5d29a1a.

--- api/assets/missile.php
+++
@@ -1,4 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/bootstrap.php';
-ravyn_asset_engine_proxy('/api/missile', ['id' => (int) ($_GET['id'] ?? 0)]);
--- api/assets/monster.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/bootstrap.php';
-$query = ['id' => (string) ($_GET['id'] ?? '')];
-if (isset($_GET['direction'])) {
-    $query['direction'] = (int) $_GET['direction'];
-}
-ravyn_asset_engine_proxy('/api/monster', $query);
